3dc60245ef0751400fe79d472b2c7ae07ba1fc34
felipealui <145783789+felipealu@users.noreply.github.com>
2024-10-29 14:03:58 -0300

    Update buscar.php

--- buscar.php
+++ buscar.php
@@ -1,9 +1,9 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "root", "loja1");
+$conn = mysqli_connect("localhost", "root", "admin", "qrvila");
 
 $nome = $_POST["nome"];
 
-$query = "SELECT nome FROM usuarios WHERE nome LIKE '%$nome%'";
+$query = "SELECT nome FROM cadastro WHERE nome LIKE '%$nome%'";
 
 $result = mysqli_query($conn, $query);
 
