999dc6486dfa811807213b11ec2421cd38479fa7
felipealui <145783789+felipealu@users.noreply.github.com>
2024-10-29 13:55:46 -0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -2,12 +2,14 @@
 const nomeField = document.getElementById("nome");
 const identificacaoField = document.getElementById("identificacao");
 const veiculoField = document.getElementById("veiculo");
+const placaField = document.getElementById("placa");
 const sitEscolaField = document.getElementById("sit_escola");
 
 // Arrays para armazenar as informações lidas
 const nomes = [];
 const identificacoes = [];
 const veiculos = [];
+const placas = [];
 const sitescola = [];
 
 function tick() {
@@ -20,17 +22,23 @@ function tick() {
       if (
         !nomes.includes(qrData.name) &&
         !identificacoes.includes(qrData.identification) &&
-        !veiculos.includes(qrData.vehicle)
+        !veiculos.includes(qrData.vehicle) &&
+        !placas.includes(qrData.placa) &&
+        !sitescola.includes(qrData.sit_escola)
       ) {
         // Armazena os novos dados nos arrays
         nomes.push(qrData.name);
         identificacoes.push(qrData.identification);
         veiculos.push(qrData.vehicle);
+        placas.push(qrData.placa);
+        sitescola.push(qrData.sit_escola);
 
         // Preenche os campos do formulário com os dados do QR code
         nomeField.value = qrData.name || "";
         identificacaoField.value = qrData.identification || "";
         veiculoField.value = qrData.vehicle || "";
+        placaField.value = qrData.placa || "";
+        sitEscolaField.value = qrData.sit_escola || "";
 
         // Atualiza a interface para mostrar os dados lidos
         outputData.innerText = "Dados preenchidos automaticamente.";
