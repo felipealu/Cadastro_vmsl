25d25cd212c4feb6ca2f2e4dd495b920282a456e
felipealui <145783789+felipealu@users.noreply.github.com>
2024-10-29 14:05:38 -0300

    Update buscar-informacoes.php

--- buscar-informacoes.php
+++ buscar-informacoes.php
@@ -1,10 +1,10 @@
 <?php
     
-    $conn = mysqli_connect("localhost", "root", "root", "loja1");
+    $conn = mysqli_connect("localhost", "root", "admin", "qrvila");
 
     $nome = $_POST["nome"];
 
-    $query = "SELECT identificacao, veiculo, placa, sit_escola FROM usuarios WHERE nome = '$nome'";
+    $query = "SELECT identificacao, veiculo, placa, sit_escola FROM cadastro WHERE nome = '$nome'";
     $result = mysqli_query($conn, $query);
 
     $data = array();
